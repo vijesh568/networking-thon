368e7bd0c5f3d525a08c0ff28634adb4b132042b
sreejithmm <sreejith.mm@gmail.com>
2015-04-02 23:12:10 +0530

    stage 2

--- server.c
+++ server.c
@@ -23,11 +23,6 @@ typedef enum vb_profile_channels_t{
 	STATIC_CHANNEL=3
 }VB_PROFILE_CHANNEL_TYPE;
 
-typedef struct server_data{
-	pthread_t server_thread_id;
-	pthread_t server_sock_id;
-
-}vb_server_data_t;
 
 typedef struct vb_server_thread_load_t{
 	int sockid;
@@ -45,9 +40,22 @@ typedef struct vb_channel_configure_data_t{
 	int option;
 }vb_channel_config_data;
 
+typedef struct vb_client_conn_prop_t {
+	int fd;
+	union {
+		struct sockaddr_in *sinaddr;
+		struct sockaddr_in6 *sin6addr;
+	}addr;
+	struct vb_client_conn_prop_t* next;
+}vb_conn_prop_st;
+
 
 vb_server_data_t server_gdt={0};
 int run_server=1;/* 1 or 0*/
+int g_vb_clients = 0;
+vb_conn_prop_st* g_vb_conn_list_head = NULL;
+vb_conn_prop_st* g_vb_conn_list_tail = NULL;
+
 
 void *get_in_addr(struct sockaddr *sa)
 {
@@ -59,85 +67,115 @@ void *get_in_addr(struct sockaddr *sa)
 			
 }
 
-int visualbox_read_data(int sockfd, int len, 
+int visualbox_read_data(int sockfd, int len);
 
-void visualbox_server_handler(void* payload)
+vb_conn_prop_st* visualbox_get_conn_list_slot()
 {
 
-	vb_common_thread_payload *load = (vb_common_thread_payload*)payload;
-	vb_server_thread_payload *data = (vb_server_thread_payload*)load->data;
-	int socketfd;
-	vb_channel_config_data readdata;
-	
-	if(data != NULL)
+	return g_vb_conn_list_tail;
+
+}
+
+void visualbox_manage_conn_prop(int new_fd, vb_conn_prop_st* conn_st)
+{
+
+	vb_conn_prop_st *loc = NULL;
+
+	conn_st->fd = new_fd;
+	if(client_addr->ss_family == AF_INET)
 	{
-
-	
-		socketfd = data->sockid;
-		while(run_server)
-		{
-
-			ret = visualbox_read_data(socketfd,sizeof(vb_channel_config_data),&readdata);
-			if(ret == 0)
-			{
-				
-			}
-
-			
-
-		
-
-		}
-	       
+		conn_st->addr.sinaddr = (struct sockaddr_in*)client_addr;
 	}
-	
-	/* listen */
-	printf("Listening\n");
-	if (listen(sockfd, 5) == -1) {
-		printf("Error listening to socket\n");
-		return -1;
+	else if(client_addr->ss_family = AF_INET6)
+	{
+			conn_st->addr.sinaddr = (struct sockaddr_in6*)client_addr;
 	}
-
-	/* accept */
-
-
-	while (1) {
-		sin_size = sizeof their_addr;
-		new_fd = accept(sockfd, (struct sockaddr *) &client_addr,
-				&sin_size);
-		if (new_fd == -1) {
-			perror("accept");
-			continue;
-		}
-
-	printf("Connection request available\n");
-		/* do operation */
-		if (fork()  !=0) {
-			close(sockfd);	// child doesn't need the listener
-			if (send(new_fd, "Hello, world!", 13, 0) == -1)
-				perror("send");
-			close(new_fd);
-			printf("exiting child\n");
-			exit(0);
-
-		}
-		close(new_fd);
-		printf("looping\n");
-
+	g_vb_clients++;
+	loc = vb_get_conn_list_slot();
+	if (!loc)
+	{
+		g_vb_conn_list_head= conn_st;
 	}
-	close(sockfd);
-	return 0;
+	else
+	{
+		loc->next = conn_st;
+		g_vb_conn_list_tail = conn_st;
+	}
+	return;
 
+}
+
+
+void visualbox_read_handler( void* payload)
+{
 
 
 
 }
+void visualbox_server_handler(void* payload)
+{
 
-int visualbox_osport_create_thread(vb_common_thread_payload* payload)
+	int sin_size;
+	int new_fd;
+	int ret = -1;
+	struct sockaddr_storage *client_addr =  NULL;
+	int socketfd;
+	
+	vb_conn_prop_st *conn_st = NULL;
+	vb_common_thread_payload *payload = NULL;
+
+	vb_common_thread_payload *load = (vb_common_thread_payload*)payload;
+	vb_server_thread_payload *data = (vb_server_thread_payload*)load->data;
+
+	
+	
+	if(data != NULL)
+	{
+		socketfd = data->sockid;
+		
+		/* listen */
+		 printf("visualbox_server_handler : INFO Listening\n");
+		 if (listen(socketfd, data->maxclients) == -1) {
+			 perror("visualbox_server_handler :Error listening to socket");
+			 return -1;
+		 }
+		
+		 while(run_server)
+		 {
+				sin_size = sizeof (struct sockaddr_storage);
+				client_addr = (struct sockaddr_storage*)malloc(sizeof(struct sockaddr_storage));
+				
+				new_fd = accept(socketfd, (struct sockaddr *) &client_addr,sin_size);
+				if (new_fd == -1) {
+					perror("visualbox_server_handler : ERROR! in accept");
+					continue;
+				}
+			
+		     	conn_st = (vb_conn_prop_st*)malloc(sizeof(vb_conn_prop_st));
+
+				visualbox_manage_conn_prop(new_fd,conn_st);
+				
+				payload->data = (void*)conn_st;
+				ret = visualbox_osport_create_thread(payload,(void*)visualbox_read_handler);
+				
+		
+		 }
+		 close(socketfd);
+
+
+
+		}
+		return 0;	       
+}
+	
+
+
+
+int visualbox_osport_create_thread(vb_common_thread_payload* payload, void* handler)
 {
 	pthread_t thread_id ;
 	
-	if (pthread_create( &thread_id , NULL ,  visualbox_server_handler , (void*) payload) !=0)
+	if (pthread_create( &thread_id , NULL ,  handler , (void*) payload) !=0)
 	{
 		perror("visualbox_osport_create_thread: ERROR: could not create thread - ");
 		return -1;
@@ -167,74 +205,15 @@ int visualbox_start_server(int sockfd,int clients_supported)
 
 	payload->data=(void*)load;
 	
-	ret = visualbox_osport_create_thread(payload);
+	ret = visualbox_osport_create_thread(payload,(void*)visualbox_server_handler);
 
 	return ret;
 
 
 }
 
-#if 0
-int main()
-{
-	struct addrinfo hints, *res, *temp;
-	int yes = 1;		/* to reuse addr */
-	int sin_size = 0;
-	struct sockaddr_storage their_addr;
-	memset(&hints, 0, sizeof(hints));
-	int sockfd,new_fd;
-	char s[INET6_ADDRSTRLEN]={'\0'};
 
-	/* fill hints */
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_flags = AI_PASSIVE;
-	hints.ai_socktype = SOCK_STREAM;
-
-
-	if (getaddrinfo(NULL, PORT, &hints, &res)) {
-		printf("Error!!!\n");
-	}
-
-	for (temp = res; temp != NULL; temp = temp->ai_next) {
-		sockfd =
-		    socket(temp->ai_family, temp->ai_socktype,
-			   temp->ai_protocol);
-		if (sockfd == -1) {
-			printf("Error!! socket creation failed\n");
-			continue;
-		}
-
-		if (setsockopt
-		    (sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-		     sizeof(int)) == -1) {
-			printf
-			    ("Error setting option to socket, errno=%d\n",
-			     errno);
-			continue;
-		}
-		if (bind(sockfd, temp->ai_addr, temp->ai_addrlen) == -1) {
-			perror("bind  failure");
-			printf("Error binding the socket to port\n");
-		        inet_ntop(temp->ai_family, get_in_addr((struct sockaddr *)temp->ai_addr),
-							            (char*) s, sizeof s);
-			printf("server:bind failed  from %s\n", s);
-			continue;
-		}
-		break;
-	}
-	if (temp == NULL) {
-		printf("bind failed\n");
-		return -1;
-
-	}
-	freeaddrinfo(res);
-
-
-
-}
-#endif
-
-int visualbox_configure_server(int clients_supported, int socktype,int port)
+int VISUALBOX_Configure_Server(int clients_supported, int socktype,int port)
 {
 	struct addrinfo hints, *res, *temp;
 	int yes = 1;		/* to reuse addr */
