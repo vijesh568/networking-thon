08f3c7ed6a3275d047c39598a7d63111c5deb5a9
sreejithmm <sreejith.mm@gmail.com>
2015-04-08 01:28:48 +0530

    making it multithreaded-stage

--- client.py
+++ client.py
@@ -2,12 +2,15 @@
 
 import socket
 import struct
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
 
-s.connect((socket.gethostname(),12323));
-buf= s.recv(1024)
-print len(buf)
-num,string,pad=struct.unpack('<I10sh',buf)
-print num
-print string
-s.close
+if __name__ == "__main__":
+	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+	servername = raw_input("Enter the ip address of the server: ")
+	port = raw_input("Enter server port number: ")
+	s.connect((servername,12323));
+	buf= s.recv(1024)
+	print "Received buffer size is "+len(buf)
+	num,string,pad=struct.unpack('<I10sh',buf)
+	print num
+	print string
+	s.close
