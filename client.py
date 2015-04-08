76753115793b9796119737cb00f4822c34e4846f
sreejithmm <sreejith.mm@gmail.com>
2015-04-08 12:31:03 +0530

    queue implementation -1

--- client.py
+++ client.py
@@ -4,26 +4,29 @@ import socket
 import struct
 import threading 
 import time
+import queue
 
 class ReaderThread(threading.Thread):
-	def __init__(self, threadID, sockID):
+	def __init__(self, threadID, sockID,queueID):
+		threading.Thread.__init__(self)
 		self.threadID = threadID
 		self.sockID = sockID
+		self.qID = queueID
+		print "init called"
 	def run(self):
 		while True:
-			self.buf = sockID.recv(1024)
+			self.buf = self.sockID.recv(1024)
 			self.num,self.string,self.pad=struct.unpack('<I10sh',self.buf)
-			print self.num
-			print self.string
-			time.delay(3)
+			q
+			time.sleep(3)
 	
 	
 if __name__ == "__main__":
 	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
 	servername = raw_input("Enter the ip address of the server: ")
-	port = raw_input("Enter server port number: ")
 	s.connect((servername,12323));
-	rdthread = ReaderThread(1,s)
+	q = queue.Queue()
+	rdthread = ReaderThread(1,s,q)
 	rdthread.start()
 	rdthread.join()
 	s.close
