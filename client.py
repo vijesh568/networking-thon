1659a2e554d98f4947507685ad75dd6bf0bd8c27
sreejithmm <sreejith.mm@gmail.com>
2015-04-08 11:10:46 +0530

    making it multithreaded-stage

--- client.py
+++ client.py
@@ -23,4 +23,5 @@ if __name__ == "__main__":
 	servername = raw_input("Enter the ip address of the server: ")
 	port = raw_input("Enter server port number: ")
 	s.connect((servername,12323));
+	rdthread = ReaderThread(1,s)
 	s.close
